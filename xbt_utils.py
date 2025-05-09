09f602f4a8a844046d19d18576b7f16b986b4539
tylryan <tlrrn5@gmail.com>
2025-05-09 04:32:25 -0500

    save: added a crapy string interpolater

--- xbt_utils.py
+++ xbt_utils.py
@@ -1,7 +1,46 @@
 
 
+def trim_quote(string: str) -> str:
+    assert isinstance(string, str)
+    return string[1:-1]
+# TODO(tyler): Put this somewhere else
+def trim_quotes(strings: list[str]) -> list[str]:
+    assert isinstance(strings, list)
+    return [trim_quote(s) for s in strings]
+
 def read_file(path: str) -> str:
     f = open(path)
     c = f.read()
     f.close()
     return c
+
+# Pretty terrible, but I'll fix later.
+def interpolate(string: str, local_env: dict[str, object]) -> str:
+    string_copy: str = trim_quote(string)
+
+    i = 0
+
+    matches: list[str] = []
+    while i < len(string) -1:
+        if string[i] == "$":
+            i+=2
+            if i >= len(string) -1:
+                break
+
+            text = ""
+            while i < len(string) - 1:
+                if string[i] == "}":
+                    break
+                text += string[i]
+                i+=1
+            matches.append(text)
+        i+=1
+    key_vals: list[tuple[str, str]] = [(f"${{{x}}}", x) for x in matches]
+
+    for key_val in key_vals:
+        key: str = key_val[0]
+        var_name: str = key_val[1]
+        new_vals: list[str] = local_env.get(var_name, var_name)
+        for v in new_vals:
+            string_copy = string_copy.replace(key, v)
+    return string_copy
