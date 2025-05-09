17ad1bdf3edd4b0b7951844c064395fa45ce01c0
tylryan <tlrrn5@gmail.com>
2025-05-09 02:26:32 -0500

    feature: added variable referencing within shell commands

--- xbt.py
+++ xbt.py
@@ -8,6 +8,13 @@ from parser.exprs import *
 
 from sys import stdout
 
+def trim_quote(string: str) -> str:
+    assert isinstance(string, str)
+    return string[1:-1]
+# TODO(tyler): Put this somewhere else
+def trim_quotes(strings: list[str]) -> list[str]:
+    assert isinstance(strings, list)
+    return [trim_quote(s) for s in strings]
 
 global global_env
 global_env = {}
@@ -39,7 +46,7 @@ def evaluate(expr: Expr, local_env: dict[str, object]) -> object:
         return None
 
     if isinstance(expr, Rule)    : return eval_rule(expr)
-    if isinstance(expr, Shell)   : return eval_shell(expr)
+    if isinstance(expr, Shell)   : return eval_shell(expr, local_env)
     if isinstance(expr,Comment)  : return eval_comment(expr)
     if isinstance(expr, Assign)  : return eval_assign(expr, local_env)
     if isinstance(expr, Literal) : return eval_literal(expr)
@@ -73,14 +80,15 @@ def eval_rule(rule: Rule) ->  None:
             # there are no redefinitions?
             rule.environment[e.variable.token.text] = eval_list(e.values, global_env)
 
-    def trim_quotes(strings: list[str]) -> list[str]:
-        return [s[1:-1] for s in strings]
 
     build_files: list[str] = rule.environment.get("build_files", [])
     output_files: list[str] = rule.environment.get("output_files", [])
 
     build_files = trim_quotes(build_files)
     output_files = trim_quotes(output_files)
+
+    import os
+    from os.path import getctime
 
     # If the user did not assign a value to one of these variables
     # then the rule should not do a time comparison and should
@@ -90,30 +98,35 @@ def eval_rule(rule: Rule) ->  None:
         rules_ran+=1
         return (True, rule_name)
 
-    import os
-    from os.path import getctime
     # At this point, the user has declared the files they wish to
     # watch. If those files don't exist, we should let them know
     # about it.
-    def files_exist_check(files: list[str]) -> None:
+    def files_exist_check(files: list[str], skip_error: bool = False) -> bool:
         for file in files:
-            if os.path.isfile(file) is False:
+            path_not_exists = os.path.isfile(file) is False
+            if path_not_exists and skip_error == False:
                 error(l, c, f"file path '{file}' in rule '{rule_name}' does not exist.")
+            if path_not_exists:
+                return False
+        return True
 
     def get_newest_file_timestamp(files: list[str]) -> int:
+        # If there are no files...
         newest_timestamp: int = getctime(files[0])
 
         for file in files[1:]:
             newest_timestamp = max(newest_timestamp, getctime(file))
         return newest_timestamp
 
-    # Make sure the files actually exist.
+    
     files_exist_check(build_files)
-    files_exist_check(output_files)
-
-
     newest_build_time: int = get_newest_file_timestamp(build_files)
-    newest_output_time: int = get_newest_file_timestamp(output_files)
+
+    # If output files do not exist, just set the program to
+    # run the rule always.
+    newest_output_time: int = 0 
+    if files_exist_check(output_files, skip_error=True):
+        newest_output_time = get_newest_file_timestamp(output_files)
 
     # If the user is watching a set of build files and any of them
     # is newer than the set of output files they are watching,
@@ -128,14 +141,34 @@ def eval_rule(rule: Rule) ->  None:
     rules_ran+=1
     return (True, rule_name)
 
-def eval_shell(shell: Shell) ->  None:
-    command: str = str(shell.commands.text)[1:].strip()
-    print(f"$ {command}")
+def eval_shell(shell: Shell, local_env: dict[str, object]) ->  None:
+    og_commands = shell.commands.text
+    commands: list[str] = str(shell.commands.text)[1:].strip().split()
+
+    # Check if the variable exists in the rule's scope.
+    # if it doesn't, then return the string as is.
+    # I'm guessing some people will want to run a command
+    # like: `$ echo $?`.
+    def resolve_variable(variable: str) -> str:
+        if variable[0] != "$": return variable
+        vars: list[str] = local_env.get(variable[1:], [])
+        if vars == []: return variable
+
+        vars = trim_quotes(vars)
+        return ' '.join(vars)
+
+
+    commands = [ resolve_variable(var) for var in commands]
+    print(f"$ {' '.join(commands)}")
 
     import os
-    res = os.popen(command)
-    [ notify(x) for x in res.readlines() ]
-    res.close()
+
+    line: int = shell.commands.line
+    return_code: int = os.system(' '.join(commands))
+    if return_code != 0:
+        print(f"\n[build-error: {line}] Exit Code {return_code}.") 
+        exit(return_code)
+
     return None
 
 def eval_comment(comment: Comment) -> None:
